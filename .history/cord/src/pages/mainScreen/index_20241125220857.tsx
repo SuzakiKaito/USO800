0ce575748a62d2faf5a800d83bf6d97f34d52d0a
SuzakiKaito <114970408+SuzakiKaito@users.noreply.github.com>
2024-11-25 22:09:31 +0900

    ボタン修正

---
+++ .history/cord/src/pages/mainScreen/index_20241125220857.tsx
@@ -0,0 +1,14 @@
+import {useEffect} from "react"
+import styles  from '@/styles/mainScreen/index.module.css';
+
+export default function main(){
+  return(
+    <>
+      <div className={styles.boxContent}>
+        <div className={styles.mainContent}>
+          <h2>嘘スレッド</h2>
+        </div>
+      </div>
+    </>
+  )
+}
